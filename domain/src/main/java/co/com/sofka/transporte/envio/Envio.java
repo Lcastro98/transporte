6760c01b28f884a8445b39a5cde50d051216d174
Willian Franco <ing.wfranco@gmail.com>
2022-05-17 15:40:36 -0500

    Adicion atributos AR - Se adicionan los atributos del AR Envio, junto con el constructor del AR

--- domain/src/main/java/co/com/sofka/transporte/envio/Envio.java
+++ domain/src/main/java/co/com/sofka/transporte/envio/Envio.java
@@ -4,7 +4,18 @@ import co.com.sofka.domain.generic.AggregateEvent;
 
 public class Envio extends AggregateEvent<EnvioId>{
 
-    public Envio(EnvioId entityId) {
+    protected final EnvioId envioId;
+    protected final Remitente remitente;
+    protected final Destinatario destinatario;
+    protected final Paquete paquete;
+    protected final Valor valor;
+
+    public Envio(EnvioId entityId, EnvioId envioId, Remitente remitente, Destinatario destinatario, Paquete paquete, Valor valor) {
         super(entityId);
+        this.envioId = envioId;
+        this.remitente = remitente;
+        this.destinatario = destinatario;
+        this.paquete = paquete;
+        this.valor = valor;
     }
 }
---
+++ domain/src/main/java/co/com/sofka/transporte/envio/Valor.java
@@ -0,0 +1,4 @@
+package co.com.sofka.transporte.envio;
+
+public class Valor {
+}
