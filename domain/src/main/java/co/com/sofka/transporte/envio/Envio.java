630f2cf87931f3f081b6ebb3c6e5f41962a3a14a
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-05-17 17:16:20 -0500

    Constructor
    Se acomoda el agregado para que esté orientado a eventos

--- domain/src/main/java/co/com/sofka/transporte/envio/Envio.java
+++ domain/src/main/java/co/com/sofka/transporte/envio/Envio.java
@@ -1,23 +1,33 @@
 package co.com.sofka.transporte.envio;
 
 import co.com.sofka.domain.generic.AggregateEvent;
+import co.com.sofka.domain.generic.DomainEvent;
+import co.com.sofka.transporte.envio.events.EnvioCreado;
 import co.com.sofka.transporte.envio.values.EnvioId;
 import co.com.sofka.transporte.envio.values.ValorComercial;
 
+import java.util.List;
+
 public class Envio extends AggregateEvent<EnvioId>{
+    protected Remitente remitente;
+    protected Destinatario destinatario;
+    protected Paquete paquete;
+    protected ValorComercial valor;
 
-    protected final EnvioId envioId;
-    protected final Remitente remitente;
-    protected final Destinatario destinatario;
-    protected final Paquete paquete;
-    protected final ValorComercial valor;
-
-    public Envio(EnvioId entityId, EnvioId envioId, Remitente remitente, Destinatario destinatario, Paquete paquete, ValorComercial valor) {
+    public Envio(EnvioId entityId, Remitente remitente, Destinatario destinatario, Paquete paquete, ValorComercial valor) {
         super(entityId);
-        this.envioId = envioId;
-        this.remitente = remitente;
-        this.destinatario = destinatario;
-        this.paquete = paquete;
-        this.valor = valor;
+        appendChange(new EnvioCreado(remitente, destinatario, paquete, valor)).apply();
+        subscribe(new EnvioEventChange(this));
+    }
+
+    private Envio(EnvioId entityId) {
+        super(entityId);
+        subscribe(new EnvioEventChange(this));
+    }
+
+    public static Envio from(EnvioId entityId, List<DomainEvent> events) {
+        var envio = new Envio(entityId);
+        events.forEach(envio::applyEvent);
+        return envio;
     }
 }
