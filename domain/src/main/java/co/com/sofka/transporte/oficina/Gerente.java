d67933dad62bbac002948a6475900213b589164a
Kr36pton <jmmesa37@misena.edu.co>
2022-05-18 00:40:51 -0500

    Se crean las entidades Gerente y Operario junto a sus IDs

--- domain/src/main/java/co/com/sofka/transporte/oficina/Gerente.java
+++ domain/src/main/java/co/com/sofka/transporte/oficina/Gerente.java
@@ -1,5 +1,20 @@
 package co.com.sofka.transporte.oficina;
 
-public class Gerente {
+import co.com.sofka.domain.generic.Entity;
+
+public class Gerente extends Entity<GerenteId>{
     
+    protected Nombre nombre;
+    protected Contacto contacto;
+    protected Salario salario;
+
+    public Gerente(GerenteId entityId, Nombre nombre, Contacto contacto, Salario salario){
+        super(entityId);
+        this.nombre = nombre;
+        this.contacto = contacto;
+        this.salario = salario;
+    }
+
+    public void actualizarContacto(){
+    }
 }
--- domain/src/main/java/co/com/sofka/transporte/oficina/Operario.java
+++ domain/src/main/java/co/com/sofka/transporte/oficina/Operario.java
@@ -1,5 +1,20 @@
 package co.com.sofka.transporte.oficina;
 
-public class Operario {
+import co.com.sofka.domain.generic.Entity;
+
+public class Operario extends Entity<OperarioId>{
+
+    protected Nombre nombre;
+    protected Contacto contacto;
+    protected Antiguedad antiguedad;
+
+    public Operario(OperarioId entityId, Nombre nombre, Contacto contacto, Antiguedad antiguedad){
+        super(entityId);
+        this.nombre = nombre;
+        this.contacto = contacto;
+        this.antiguedad = antiguedad;
+    }
     
+    public void actualizarContacto(){
+    }
 }
