2abe8889b60c49b65b552e8e43c150592112f597
Kr36pton <jmmesa37@misena.edu.co>
2022-05-17 23:34:36 -0500

    Se agregan atributos y el constructor del AR Oficina

--- domain/src/main/java/co/com/sofka/transporte/oficina/Oficina.java
+++ domain/src/main/java/co/com/sofka/transporte/oficina/Oficina.java
@@ -4,8 +4,20 @@ import co.com.sofka.domain.generic.AggregateEvent;
 
 public class Oficina extends AggregateEvent<OficinaId> {
 
-    public Oficina(OficinaId entityId){
-        super(entityId);
-    }
+    protected final OficinaId oficinaId;
+    protected final Nombre nombre;
+    protected final Ubicacion ubicacion;
+    protected final Contacto contacto;
+    protected final Gerente gerente;
+    protected final Operario operario;
 
+    public Oficina(OficinaId entityId, OficinaId oficinaId, Nombre nombre, Ubicacion ubicacion, Contacto contacto, Gerente gerente, Operario operario){
+        super(entityId);
+        this.oficinaId = oficinaId;
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+        this.contacto = contacto;
+        this.gerente = gerente;
+        this.operario = operario;
+    }
 }
