1bb8e24c278d29eac12b359bc042b18297919f95
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-05-17 21:18:15 -0500

    Event Change
    Se agregan los eventos para actualizar remitente y destinatario

--- domain/src/main/java/co/com/sofka/transporte/envio/EnvioEventChange.java
+++ domain/src/main/java/co/com/sofka/transporte/envio/EnvioEventChange.java
@@ -1,8 +1,10 @@
 package co.com.sofka.transporte.envio;
 
 import co.com.sofka.domain.generic.EventChange;
+import co.com.sofka.transporte.envio.events.DestinatarioActualizado;
 import co.com.sofka.transporte.envio.events.DestinoAgregado;
 import co.com.sofka.transporte.envio.events.EnvioCreado;
+import co.com.sofka.transporte.envio.events.RemitenteActualizado;
 
 public class EnvioEventChange extends EventChange {
     public EnvioEventChange(Envio envio) {
@@ -15,6 +17,16 @@ public class EnvioEventChange extends EventChange {
 
         apply((DestinoAgregado event) -> {
             envio.destino = event.getValue();
+        });
+
+        apply((RemitenteActualizado event) -> {
+            envio.remitente.nombre = event.getNombre();
+            envio.remitente.contacto = event.getContacto();
+        });
+
+        apply((DestinatarioActualizado event) -> {
+            envio.destinatario.nombre = event.getNombre();
+            envio.destinatario.contacto = event.getContacto();
         });
     }
 }
