502c048c25a3159929c68fd0cc0e0b5559cfbad5
pablo-davila1 <62685470+pablo-davila1@users.noreply.github.com>
2022-05-17 23:04:49 -0500

    Ordenamiento
    Se crean el primer evento, y se incluyen en los paquetes pertinentes.

--- transporte/domain/src/main/java/co/com/sofka/transporte/mensajero/value/Contacto.java
+++ transporte/domain/src/main/java/co/com/sofka/transporte/mensajero/value/Contacto.java
@@ -1,6 +1,7 @@
 package co.com.sofka.transporte.mensajero.value;
 
 import co.com.sofka.domain.generic.AggregateEvent;
+import co.com.sofka.transporte.mensajero.event.ContactoCreado;
 
 import java.util.Set;
 
@@ -12,7 +13,9 @@ public class Contacto extends AggregateEvent<ContactoId> {
     protected Telefono telefono;
     protected Set<Envio> envio;
 
-    public Contacto(ContactoId entityId) {
+    public Contacto(ContactoId entityId, Nombre nombre,Ciudad cuidad, Direccion direccion,Telefono telefono,MedioTransporte medioTransporte) {
         super(entityId);
+        appendChange(new ContactoCreado(nombre, cuidad, direccion, telefono, medioTransporte)).apply();
+        subscribe(new ContactoEventChange(this));
     }
 }
--- transporte/domain/src/main/java/co/com/sofka/transporte/mensajero/value/MedioTransporte.java
+++ transporte/domain/src/main/java/co/com/sofka/transporte/mensajero/value/MedioTransporte.java
@@ -3,7 +3,7 @@ package co.com.sofka.transporte.mensajero.value;
 import co.com.sofka.domain.generic.Entity;
 
 public class MedioTransporte extends Entity<MedioTransporteId> {
-    
+
     public MedioTransporte(MedioTransporteId entityId) {
         super(entityId);
     }
